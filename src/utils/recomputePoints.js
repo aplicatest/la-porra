be709d73cbc421404cef5849b7a2bc5656c7c344
Marcos <marcosrecuencocava07@gmail.com>
2026-08-25 14:57:08 +0200

    Forzar lectura del servidor al recalcular puntos, para evitar carreras al volcar varios pronosticos seguidos

--- src/utils/recomputePoints.js
+++ src/utils/recomputePoints.js
@@ -1,9 +1,13 @@
-import { collection, getDocs, query, where, writeBatch } from 'firebase/firestore'
+import { collection, getDocsFromServer, query, where, writeBatch } from 'firebase/firestore'
 import { db } from '../firebase'
 import { calculatePoints } from './scoring'
 
 export async function recomputeMatchPoints(matchId, homeGoals, awayGoals) {
-  const predictionsSnap = await getDocs(
+  // getDocsFromServer (en vez de getDocs) fuerza a leer del servidor siempre,
+  // para no arriesgarse a que un pronostico recien guardado un instante antes
+  // (p.ej. al volcar varios a mano seguidos) no aparezca aun en una respuesta
+  // servida desde cache local.
+  const predictionsSnap = await getDocsFromServer(
     query(collection(db, 'predictions'), where('matchId', '==', matchId))
   )
 
